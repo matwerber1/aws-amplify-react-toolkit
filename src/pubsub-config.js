44b74bf423e027d06c30760143ef77d8a7fb3552
gannaramu <ganna.ramu@gmail.com>
2020-10-31 19:02:01 -0600

    Update pubsub-config.js

--- src/pubsub-config.js
+++ src/pubsub-config.js
@@ -1,6 +1,6 @@
 const pubsub_config = {
   "iot_endpoint": 'wss://aiz9usfggyt4s-ats.iot.us-east-1.amazonaws.com/mqtt',
-  "iot_policy": "GenericPolicy",
+  "iot_policy": "myIoTPolicy",
   "iot_region": "us-east-1"
 };
 
