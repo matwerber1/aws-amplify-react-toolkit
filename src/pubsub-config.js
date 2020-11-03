60f54e6ec8e57849c36e4d1c52a83cf77c40a6b8
admin <admin@admins-MacBook-Pro.local>
2020-11-03 11:12:08 -0800

    delete pubsub-config.js

--- src/pubsub-config.js
+++
@@ -1,7 +0,0 @@
-const pubsub_config = {
-  "iot_endpoint": 'wss://a2mvse6841elo7-ats.iot.us-west-2.amazonaws.com/mqtt',
-  "iot_policy": "ReactIoTPolicy",
-  "iot_region": "us-west-2"
-};
-
-export default pubsub_config;
